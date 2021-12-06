3664a5df5777417b2c8a0d35f90841a874b16e4f
Stephen Reaves <sreaves@redhat.com>
2021-12-06 17:06:38 -0500

    Day 2

---
+++ 2021/Day02/2/main.go
@@ -0,0 +1,75 @@
+/**
+ * File: main.go
+ * Written by: Stephen M. Reaves
+ * Created on: Mon, 06 Dec 2021
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	filename = "../input.txt"
+)
+
+type movement struct {
+	direction string
+	scale     int
+}
+
+var movements []movement
+
+func readFile(r io.Reader) error {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		v := strings.Split(scanner.Text(), " ")
+		fmt.Println(v[1], v[0])
+		scale, err := strconv.Atoi(v[1])
+		if err != nil {
+			return err
+		}
+		movements = append(movements, movement{
+			direction: v[0],
+			scale:     scale,
+		})
+	}
+	return nil
+}
+
+func main() {
+	reader, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = readFile(reader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	forward, depth, aim := 0, 0, 0
+	for _, v := range movements {
+		switch v.direction {
+		case "forward":
+			forward += v.scale
+			depth += v.scale * aim
+		case "up":
+			aim -= v.scale
+		case "down":
+			aim += v.scale
+		}
+	}
+	fmt.Println("forward", forward)
+	fmt.Println("depth", depth)
+	fmt.Println("Total number", forward*depth)
+	time.Sleep(10 * time.Second)
+}
